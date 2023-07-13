9f8e1f233e92870ea871ee0e6dff8dd249bd65d9
Petrando <bangkitwisata@gmail.com>
2023-07-13 20:54:54 +0800

    feat:added scroll to top arrow

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,18 +1,28 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, FunctionComponent } from 'react';
 import { usePathname } from 'next/dist/client/components/navigation';
 import { SessionProvider } from 'next-auth/react';
+import ScrollToTop from 'react-scroll-to-top';
 import { CartContext } from '@/context/cartContext';
 import { itemTotal } from '@/utils/helpers/cart-helper';
 import { ShoppingCart } from '@/components/Cart';
 import { DeleteProductDialog } from '@/components/admin/manage-products/delete-product';
+import { ArrowSmallUp } from '@/components/Icons';
 import { IProduct } from '../../types';
 type Props = {
   children?: React.ReactNode;
 };
 
-export const NextEcommerceProvider = ({ children }: Props) => {
+const ScrollArrow:FunctionComponent = () => {
+  return (
+      <div className="bg-zinc-800 text-white h-full rounded-lg flex items-center justify-center">
+          <ArrowSmallUp />
+      </div>
+  );
+}
+
+export const NextEcommerceProvider:FunctionComponent = ({ children }: Props) => {
     const [itemCount, setCount] = useState(0);
     const [ productToDelete, setToDelete ] = useState<IProduct | null>(null); 
 
@@ -37,8 +47,11 @@ export const NextEcommerceProvider = ({ children }: Props) => {
                 <DeleteProductDialog
                     product={productToDelete}
                     closeDialog={()=>{setToDelete(null);}}
-                />
-                
+                />                
+              }
+              {
+                (pathname !== "/login" && pathname !== "/register" && pathname !== "/about-me") &&
+                    <ScrollToTop smooth component={<ScrollArrow />}/>
             }
           </CartContext.Provider>
         </SessionProvider>;
